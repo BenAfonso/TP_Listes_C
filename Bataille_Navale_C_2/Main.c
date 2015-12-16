03bbcfec3c5e0535782faa2fa79f53e297389d79
Darkyler <darkyler57@gmail.com>
2015-12-16 14:31:37 +0100

    ...

--- Bataille_Navale_C_2/Main.c
+++ Bataille_Navale_C_2/Main.c
@@ -11,34 +11,54 @@ int main (void)
 
     printf("John Cena welcomes you to Bataillette Navale 2015. In the most mindblowing, balltwisting battleship match of the century, two players will face their destinies in the arena of death. \n");
 
-    PartieN partie = creerPartie();          /* crée une partie appelée "partie" */
-
-    JoueurN j1 = joueur1(partie);            /* initialise une variable j1 correspondant au joueur 1 de la partie */
-    JoueurN j2 = joueur2(partie);            /* initialise une variable j2 correspondant au joueur 2 de la partie */
-
-    partie = commencerPartie(partie);            /* modifie la partie en appelant commencerPartie(Partie) -> rend j1 actif et laisse j2 inactif */
+/* crée une partie appelée "partie" */
+/*
+    PartieN partie = creerPartie();
+*/
+/* initialise une variable j1 correspondant au joueur 1 de la partie */
+/*
+    JoueurN j1 = joueur1(partie);
+*/
+/* initialise une variable j2 correspondant au joueur 2 de la partie */
+/*
+    JoueurN j2 = joueur2(partie);
+*/
+ /* modifie la partie en appelant commencerPartie(Partie) -> rend j1 actif et laisse j2 inactif */
+/*
+    partie = commencerPartie(partie);
 
     printf("Player 1 needs to place his ships \n");
-    placerBateau(j1);                                   /* demande à j1 de placer ses bateaux */
+*/
+/* demande à j1 de placer ses bateaux */
+/*
+    placerBateau(j1);
     printf("Player 2 needs to place his ships \n");
-    placerBateau(j2);                                   /* demande à j2 de placer ses bateaux */
-
-    while((estFinie(partie))==0)            /* tant que la partie n'est pas terminé -> la flotte de j1 ou j2 n'est pas vide */
+*/
+/* demande à j2 de placer ses bateaux */
+/*
+    placerBateau(j2);
+*/
+/* tant que la partie n'est pas terminé -> la flotte de j1 ou j2 n'est pas vide */
+/*
+    while((estFinie(partie))==0)
     {
-        partie = tirer(partie);                 /* le joueur actif tire sur le joueur inactif et modifie la partie si nécessaire */
-        partie = changerJoueurActif(partie);    /* modifie la partie en inversant l'état de chaque joueur */
-    }
 
+        partie = tirer(partie);
+        partie = changerJoueurActif(partie);
+    }
+*/
     /* une des flottes est vide */
 
-    if (estVideFlotte(flotte(j1)) == 1) {               /* si la flotte de j1 est vide */
+/* si la flotte de j1 est vide */
+/*
+    if (estVideFlotte(flotte(j1)) == 1) {
         printf("GAME OVER! Player 2 is the winner.");
     }
-    else                                             /* si la flotte de j2 est vide */
+    else
     {
         printf("GAME OVER! Player 1 is the winner.");
     }
-
+*/
     return 0;
 
 }
