7c482cc9d0c52f654f6f23e30e46c8ab30d3cd6f
Alexandre Bouthinon <bouthinon.alexandre@gmail.com>
2015-12-16 14:59:17 +0100

    Ajout Flotte.c

---
+++ Bataille_Navale_C_2/Flotte.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "Flotte.h"
+#include "Bateau.h"
+
+FlotteN creerFlotte()
+{
+    return NULL;
+}
+
+int estVideFlotte(FlotteN f)
+{
+    if (f->nbreBateaux = 0)
+    {
+        return 1;
+    }
+    else
+    {
+        return 0;
+    }
+}
